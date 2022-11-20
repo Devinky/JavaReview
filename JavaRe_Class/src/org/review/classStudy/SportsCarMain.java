f08acea98fbd1c1df8bb76cce1f748752f0a7b74
Devinky <devinky01@gmail.com>
2022-11-20 23:11:42 +0900

    221120 Field,Method

---
+++ JavaRe_Class/src/org/review/classStudy/SportsCarMain.java
@@ -0,0 +1,13 @@
+package org.review.classStudy;
+
+public class SportsCarMain {
+	
+	public static void main(String[] args) {
+		SportsCar car = new SportsCar("붕붕",2022,1000,"Blue");
+		System.out.println(car.carName);
+		System.out.println(car.carYear);
+		System.out.println(car.carPrice);
+		System.out.println(car.carColor);
+	}
+
+}
